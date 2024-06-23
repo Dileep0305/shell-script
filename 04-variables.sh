5b9a56de14efff280303561180c07ada2b0defd1
Dileep0305 <prince2dilip@gmail.com>
2024-06-23 23:07:46 +0530

    3rd variable shell script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: No response"
+echo "$PERSON1: Please help me $PERSON2"
+echo "$PERSON2: Sure $PERSON1 and problem fixed"
