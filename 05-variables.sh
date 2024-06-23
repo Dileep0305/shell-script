edc5ce2930da73824dbde091595215d82d334bf9
Dileep0305 <prince2dilip@gmail.com>
2024-06-23 23:16:52 +0530

    4rd variable shell script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+echo "Please enter the username:"
+
+read -s USERNAME
+
+echo "Thank you, could you also enter the password:"
+
+read -s PASSWORD
+
+echo "Please find the credentials of user:$USERNAME and password:$PASSWORD"
