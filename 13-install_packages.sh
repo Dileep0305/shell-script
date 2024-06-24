5a86b98f53410e8085c40a7bb4390b084f606894
Dileep0305 <prince2dilip@gmail.com>
2024-06-24 17:15:49 +0530

    packages installation shell script

--- 13-install_packages.sh
+++ 13-install_packages.sh
@@ -10,7 +10,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script started executing at $TIMESTAMP" &>>$LOGFILE
 
-if [ $? -ne 0 ]; then
+if [ $ID -ne 0 ]; then
     echo -e "$R Error: Please run the script with user access $N"
     exit 1
 else
@@ -30,7 +30,7 @@ for package in $@; do
     yum list installed $package $ >>$LOGFILE
     if [ $? -ne 0 ]; then
         yum install $package -y $ >>$LOGFILE
-        validate $? "Installing of $package"
+        validate $? "Installing of $package "
     else
         echo -e "$package is already installed ... $Y skipping $N"
     fi
