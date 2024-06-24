c57f3ff7ef3437a9168ff990e772316acffe1a23
Dileep0305 <prince2dilip@gmail.com>
2024-06-24 17:19:54 +0530

    packages installation shell script

--- 13-install_packages.sh
+++ 13-install_packages.sh
@@ -1,37 +1,41 @@
-#!bin/bash
+#!/bin/bash
 
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-TIMESTAMP=$(date +%F-%M-%H-%S)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" &>>$LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>>$LOGFILE
 
-if [ $ID -ne 0 ]; then
-    echo -e "$R Error: Please run the script with user access $N"
-    exit 1
-else
-    echo "You are root user"
-fi
-
-validate() {
+VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo -e "Error: $2 is $R failed $N"
-        exit 1
+        echo -e "$2 ... $R FAILED $N"
     else
-        echo -e " $2 is $G success $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
+if [ $ID -ne 0 ]; then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
+
 for package in $@; do
-    yum list installed $package &>>$LOGFILE
-    if [ $? -ne 0 ]; then
-        yum install $package -y &>>$LOGFILE
-        validate $? "Installing of $package "
+    yum list installed $package &>>$LOGFILE    #check installed or not
+    if [ $? -ne 0 ]; then                      #if not installed
+        yum install $package -y &>>$LOGFILE    # install the package
+        VALIDATE $? "Installation of $package" # validate
     else
-        echo -e "$package is already installed ... $Y skipping $N"
+        echo -e "$package is already installed ... $Y SKIPPING $N"
     fi
 done
